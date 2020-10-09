10efd67e9e307dfe1fe0c67a16b84f70d0c3c0de
c.rojas.santiago@gmail.com <c.rojas.santiago@gmail.com>
2020-10-09 17:41:04 -0300

    install

---
+++ amsa-form/src/components/SelEmpresas.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="largo-empresas">
+        <!--Datos de la empresa-->
+        <!--Empresa-->
+        <b-row class="justify-content-center">
+            <b-col class="box-respuestas-1" style="min-width: 325px" cols="12" md="6">
+                <md-card class="md-layout-item box-respuestas-1">
+                    <md-badge id="badge-steps" md-content="5"/>
+                    <md-card-header>
+                        <div class="md-title">
+                            <md-icon class="fa fa-briefcase md-size-2x"></md-icon>
+                            <span style="margin-left: 10px;">Empresa</span>
+                        </div>
+                    </md-card-header>
+                    <md-card-content class="bottom">
+                        <md-field>
+                            <label for="empresa">Empresa</label>
+                            <md-select name="empresa" id="empresa" v-model="empresa" md-dense>
+                                <md-option v-for="emp of empresas" :key="emp" :value="emp">
+                                    {{ emp }}
+                                </md-option>
+                            </md-select>
+                        </md-field>
+                    </md-card-content>
+                </md-card>
+            </b-col>
+            <b-col class="box-respuestas-1" style="min-width: 325px" cols="12" md="6">
+                <md-card class="md-layout-item box-respuestas-1">
+                    <md-badge id="badge-steps" md-content="6"/>
+                    <md-card-header>
+                        <div class="md-title">
+                            <md-icon class="fa fa-briefcase md-size-2x"></md-icon>
+                            <span style="margin-left: 10px;">Piso</span>
+                        </div>
+                    </md-card-header>
+                    <md-card-content class="bottom">
+                        <md-field>
+                            <label for="area">Piso de destino</label>
+                            <md-select name="area" id="area" v-model="Area" md-dense>
+                                <md-option v-for="ar of areas" :key="ar" :value="ar">
+                                    {{ ar }}
+                                </md-option>
+                            </md-select>
+                        </md-field>
+                    </md-card-content>
+                </md-card>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+import {empresas, areas} from '../variables.js'
+export default {
+    name: "SelEmpresas",
+    data() {
+        return {
+            empresas,
+            areas,
+            area: null,
+            empresa: null
+        }
+    }
+}
+</script>
+
+<style scoped>
+.largo-empresas{
+    width: 80%;
+    padding: 0 15px
+}
+@media screen and (max-width: 800px){
+    .largo-empresas{
+    width: 100%;
+    padding: 0;
+}
+}
+</style>
---
+++ amsa-form/src/components/b-day.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="b-day">
+        <!--Fecha de nacimiento-->
+        <b-row class="justify-content-center">
+            <b-col class="box-respuestas-1" cols="12" md="6">
+                <md-card class="md-layout-item box-respuestas-1">
+                    <md-badge id="badge-steps" md-content="4"/>
+                    <md-card-header>
+                        <div class="md-title">
+                            <md-icon class="fa fa-calendar md-size-2x"></md-icon>
+                            <span>Fecha de nacimiento</span>
+                        </div>
+                    </md-card-header>
+                    <md-card-content class="bottom">
+                        <md-field>
+                        <datepicker placeholder="Select Date" v-model="vModelExample"></datepicker>
+                        <!--<md-input type="fecha" name="fecha" id="fecha"  autocomplete="given-fecha" v-model="numeroTel"/>-->
+                        </md-field>
+                    </md-card-content>
+                </md-card>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Selcumple",
+    data() {
+        return {
+            cumple:null,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.b-day{
+    width: 80%;
+    padding: 0 15px
+}
+@media screen and (max-width: 800px){
+    .b-day{
+    width: 100%;
+    padding: 0;
+}
+}
+</style>
